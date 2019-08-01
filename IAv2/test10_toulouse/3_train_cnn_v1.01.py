644e6b17855e9a524cc320163e5b3a8bc577028f
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-08-01 07:59:46 +0200

    Full CONV test

---
+++ IAv2/test10_toulouse/3_train_cnn_v1.01.py
@@ -0,0 +1,331 @@
+import numpy as np
+import math
+import cv2
+import matplotlib.pyplot as plt
+import random
+from datetime import datetime
+import h5py
+from sklearn.utils import shuffle
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers import SpatialDropout2D
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.utils import plot_model
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
+# parameters
+width = 160 # 1280 / 8
+height = 32 #
+inmage_shape = (height, width,1)
+param_units_hidden_1 = 128 #128 ***
+param_units_hidden_2 = 32 #128
+
+# hyperparameters
+hyp_epoch = 30 #300
+hyp_bs = 128 #128
+hyp_lr = 0.0001 #0.00005
+hyp_lr_decay = 0.0 #0.0
+hyp_l2_regularization = 0.0001 #0.0001
+hyp_weight_dropout_1 = 0.50 #0.50
+hyp_weight_dropout_2 = 0.20 #0.20
+
+##Epoch 100/100 avec 5x5
+## - 1s - loss: 0.0103 - mean_squared_error: 0.0051 - val_loss: 0.0071 - val_mean_squared_error: 0.0019
+##train 
+##mean_squared_error: 0.0015
+##test 
+##mean_squared_error: 0.0019
+
+##Epoch 300/300 avec 7x7
+## - 1s - loss: 0.0067 - mean_squared_error: 0.0027 - val_loss: 0.0049 - val_mean_squared_error: 0.0011
+##train 
+##mean_squared_error: 0.0005
+##test 
+##mean_squared_error: 0.0011
+
+random.seed(datetime.now())
+
+# load dataset
+X = np.transpose(np.loadtxt("Xtrain.txt", dtype=float))
+Y = np.transpose(np.loadtxt('Ytrain.txt', dtype=float))
+m = X.shape[0]
+print(str(m))
+
+# shuffle dataset (mini batch)
+X_shuffled, Y_shuffled = shuffle(X, Y)
+
+# split dataset
+m_train = math.floor(m*0.80)
+print(str(m_train))
+X_train = X_shuffled[:m_train,:].reshape(m_train,height,width,1)
+Y_train = Y_shuffled[:m_train]
+X_test = X_shuffled[m_train:,:].reshape(m-m_train,height,width,1)
+Y_test = Y_shuffled[m_train:]
+
+# create model
+model = Sequential()
+
+# CONV 30epochs 
+model.add(Conv2D(8, kernel_size=(1, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 1)))
+#160x16
+model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+#80x8
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+#40x4
+model.add(Conv2D(4, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+#20x2
+
+# CONV 100epochs 20 26
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(1, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(1, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# CONV 100epochs 18 25
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(1, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# CONV 100epochs 10 26
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# CONV 30epochs 16 18
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+
+
+# CONV 30epochs 22 22
+##model.add(Conv2D(8, kernel_size=(3, 3), strides=(1, 2), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(8, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(5, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+
+# CONV 30epochs 31 37
+##model.add(Conv2D(8, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(8, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+
+# CONV 30epochs 18 19
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+
+# CONV 30epochs 16 30
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+
+# CONV 24 30
+##model.add(Conv2D(8, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(8, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# CONV 31 39
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# CONV 25 43
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 29 / 40
+##model.add(Conv2D(8, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 32 36 / 40 43
+##model.add(Conv2D(8, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 32 36
+##model.add(Conv2D(8, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 48 56
+##model.add(Conv2D(8, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(2, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 49 59
+##model.add(Conv2D(8, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(1, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 58 64
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(16, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(1, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 53 75
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+##model.add(Conv2D(8, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 49 86
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(4, kernel_size=(7, 7), strides=(1, 1), dilation_rate = (2,1), border_mode='same', activation='relu'))
+##model.add(MaxPooling2D(pool_size=(2, 1)))
+
+# CONV 78 64
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(MaxPooling2D(pool_size=(2, 2)))
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(AveragePooling2D(pool_size=(2, 2)))
+
+# CONV 84 113
+##model.add(Conv2D(3, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+
+# CONV 36 51
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (2,2), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(Conv2D(4, kernel_size=(5, 5), strides=(1, 1), dilation_rate = (1,1), border_mode='same', activation='relu', input_shape=inmage_shape))
+##model.add(AveragePooling2D(pool_size=(2, 1)))
+
+# CONV -> FC
+model.add(Flatten())
+
+# FCN
+#model.add(Dropout(hyp_weight_dropout_1))
+#model.add(Dense(param_units_hidden_1,activity_regularizer=l2(hyp_l2_regularization)))
+#model.add(Activation("relu"))
+
+model.add(Dropout(hyp_weight_dropout_2))
+model.add(Dense(param_units_hidden_2,activity_regularizer=l2(hyp_l2_regularization)))
+model.add(Activation("relu"))
+
+model.add(Dropout(hyp_weight_dropout_2))
+model.add(Dense(1,activity_regularizer=l2(hyp_l2_regularization))) # 1 output
+model.add(Activation("linear"))
+
+# summarize model.
+model.summary()
+
+# Compile model
+opt = Adam(lr=hyp_lr,decay=hyp_lr_decay)
+model.compile(loss='mean_squared_error', optimizer=opt, metrics=['mse'])
+# Fit the model
+history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=hyp_epoch, batch_size=hyp_bs, verbose=2)
+# evaluate the model
+scores = model.evaluate(X_train, Y_train, verbose=0)
+print("train \n%s: %.4f" % (model.metrics_names[1], scores[1]))
+# evaluate the model
+scores = model.evaluate(X_test, Y_test, verbose=0)
+print("test \n%s: %.4f" % (model.metrics_names[1], scores[1]))
+
+# list all data in history
+print(history.history.keys())
+# plot history
+#plt.semilogy(history.history['acc'], label='train')
+#plt.semilogy(history.history['val_acc'], label='test')
+plt.plot(history.history['mean_squared_error'], label='train')
+plt.plot(history.history['val_mean_squared_error'], label='test')
+plt.ylabel('mean_squared_error')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+
+# save model and architecture to single file
+model.save("model.h5")
+print("Saved model to disk")
+
+# summarize model.
+plot_model(model, to_file="model.png")
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
---
+++ IAv2/test10_toulouse/4_predict_cnn_v0.01.py
@@ -0,0 +1,135 @@
+import numpy as np
+import math
+import cv2
+import matplotlib.pyplot as plt
+from keras import models
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.optimizers import Adam
+from keras.models import load_model
+import random
+from datetime import datetime
+import h5py
+from sklearn.utils import shuffle
+from keras.regularizers import l2
+import os
+
+def image2vector(image):
+    """
+    Argument:
+    image -- a numpy array of shape (length, height, depth)
+    
+    Returns:
+    v -- a vector of shape (length*height*depth, 1)
+    """
+    size = image.shape[0] * image.shape[1] * image.shape[2]
+    v = image.reshape(size, 1)
+    return v
+
+# parameters
+
+# load model
+model = load_model('model.h5')
+print("Loaded model from disk")
+# summarize model.
+model.summary()
+# internal vizualisation
+layer_count = 9
+layer_outputs = [layer.output for layer in model.layers[:layer_count]] # Extracts the outputs of the top 4 layers
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs) # Creates a model that will return these outputs, given the model input
+layer_names = []
+for layer in model.layers[:layer_count]:
+    layer_names.append(layer.name) # Names of the layers, so you can have them as part of your plot
+images_per_row = 1
+
+# input picture
+width = 160 # 1280 / 8
+height = 90 # 720 / 8
+crop_height = 32 # power of 2
+height_start_1 = 10 # low offset from bottom
+height_end_1 = height_start_1+crop_height # high offset from bottom
+height_start_2 = 42
+height_end_2 = height_start_2+crop_height
+
+# load images
+list = os.listdir('images_test/')
+for i in list:
+    print("images_test/"+i)
+    image = cv2.imread("images_test/"+i,cv2.IMREAD_COLOR)
+    assert(image.shape == (720,1280,3))
+    # resize
+    dim = (width,height)
+    im_resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    assert(im_resized.shape == (height,width,3))
+    # crop
+    im_croped_1 = im_resized[height-height_end_1:height-height_start_1,:width]
+    assert(im_croped_1.shape == (crop_height,width,3))
+    im_croped_2 = im_resized[height-height_end_2:height-height_start_2,:width]
+    assert(im_croped_2.shape == (crop_height,width,3))
+    # build x
+    #x_1 = image2vector(im_croped_1)
+    #x_2 = image2vector(im_croped_2)
+    x_1 = im_croped_1
+    x_2 = im_croped_2
+    #assert(x_1.shape == (height*width*3,1))
+    #assert(x_2.shape == (height*width*3,1))
+    assert(x_1.shape == (crop_height,width,3))
+    assert(x_2.shape == (crop_height,width,3))
+    #make a prediction
+    #y_1 = model.predict(np.transpose(x_1/255.0))
+    #y_2 = model.predict(np.transpose(x_2/255.0))
+    y_1 = model.predict(x_1.reshape(1,crop_height,width,3)/255.0)
+    y_2 = model.predict(x_2.reshape(1,crop_height,width,3)/255.0)
+    print(str(y_1))
+    print(str(y_2))
+    # internal vizualisation
+    activations = activation_model.predict(x_1.reshape(1,crop_height,width,3)/255.0)     # Returns a list of five Numpy arrays: one array per layer activation
+    for layer_name, layer_activation in zip(layer_names, activations): # Displays the feature maps
+        n_features = layer_activation.shape[-1] # Number of features in the feature map
+        size_h = layer_activation.shape[1] #The feature map has shape (1, size, size, n_features).
+        size_w = layer_activation.shape[2] #The feature map has shape (1, size, size, n_features).
+        #print(str(layer_activation.shape[0]))
+        #print(str(layer_activation.shape[1]))
+        #print(str(layer_activation.shape[2]))
+        n_cols = n_features // images_per_row # Tiles the activation channels in this matrix
+        display_grid = np.zeros((size_h * n_cols, images_per_row * size_w))
+        for col in range(n_cols): # Tiles each filter into a big horizontal grid
+            for row in range(images_per_row):
+                channel_image = layer_activation[0,
+                                             :, :,
+                                             col * images_per_row + row]
+                channel_image -= channel_image.mean() # Post-processes the feature to make it visually palatable
+                channel_image /= channel_image.std()
+                channel_image *= 64
+                channel_image += 128
+                channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+                display_grid[col * size_h : (col + 1) * size_h, # Displays the grid
+                         row * size_w : (row + 1) * size_w] = channel_image
+        scale = 1. / size_h
+        plt.clf()
+        plt.figure(figsize=(scale * display_grid.shape[1],
+                        scale * display_grid.shape[0]))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
+        plt.savefig('predict/'+i+'_'+layer_name+'.png')
+        #plt.show()
+        #plt.pause(1)
+        plt.close('all')
+
+    
+    plt.clf()
+    plt.imshow(image)
+    plt.plot([(y_1[0]+1.0)/2.0*1280, (y_2[0]+1.0)/2.0*1280], [(height-height_start_1-crop_height/2)*8,(height-height_end_2+crop_height/2)*8],linewidth=5)
+    plt.savefig('predict/'+i+'_overlay.png')
+    #plt.show()
+    plt.pause(1)
+    #plt.close('all')
+
+
+
+
+
+
+
