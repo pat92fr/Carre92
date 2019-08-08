df261328fbf4391c177250d4d9ccdde107cd42d8
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-08-08 23:51:59 +0200

    Gen

--- Alain/ExtractionFond/auto.bat
+++ Alain/ExtractionFond/auto.bat
@@ -1,12 +1,13 @@
-ExtractionFond.py IMG_20190808_171156.jpg 100
-ExtractionFond.py IMG_20190808_171205.jpg 100
-ExtractionFond.py IMG_20190808_171217.jpg 100
-ExtractionFond.py IMG_20190808_171229.jpg 100
-ExtractionFond.py IMG_20190808_171243.jpg 100
-ExtractionFond.py IMG_20190808_171335.jpg 100
-ExtractionFond.py IMG_20190808_171344.jpg 100
-ExtractionFond.py IMG_20190808_171354.jpg 100
-ExtractionFond.py IMG_20190808_171406.jpg 100
-ExtractionFond.py IMG_20190808_171411.jpg 100
-ExtractionFond.py IMG_20190808_171417.jpg 100
+ExtractionFond.py IMG_20190808_171417.jpg 350
+ExtractionFond.py IMG_20190808_171156.jpg 350
+ExtractionFond.py IMG_20190808_171205.jpg 350
+ExtractionFond.py IMG_20190808_171217.jpg 350
+ExtractionFond.py IMG_20190808_171229.jpg 350
+ExtractionFond.py IMG_20190808_171243.jpg 350
+ExtractionFond.py IMG_20190808_171335.jpg 350
+ExtractionFond.py IMG_20190808_171344.jpg 350
+ExtractionFond.py IMG_20190808_171354.jpg 350
+ExtractionFond.py IMG_20190808_171406.jpg 350
+ExtractionFond.py IMG_20190808_171411.jpg 350
+
 
