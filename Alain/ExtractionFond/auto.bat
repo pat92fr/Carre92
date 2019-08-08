41acac0b813dab9b4fdd95a29d705f6797d3f914
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-08-08 19:56:14 +0200

    Alain script

---
+++ Alain/ExtractionFond/auto.bat
@@ -0,0 +1,12 @@
+ExtractionFond.py IMG_20190808_171156.jpg 100
+ExtractionFond.py IMG_20190808_171205.jpg 100
+ExtractionFond.py IMG_20190808_171217.jpg 100
+ExtractionFond.py IMG_20190808_171229.jpg 100
+ExtractionFond.py IMG_20190808_171243.jpg 100
+ExtractionFond.py IMG_20190808_171335.jpg 100
+ExtractionFond.py IMG_20190808_171344.jpg 100
+ExtractionFond.py IMG_20190808_171354.jpg 100
+ExtractionFond.py IMG_20190808_171406.jpg 100
+ExtractionFond.py IMG_20190808_171411.jpg 100
+ExtractionFond.py IMG_20190808_171417.jpg 100
+
