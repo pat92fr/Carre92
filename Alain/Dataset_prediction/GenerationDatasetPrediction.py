1aa21b400e11122116bc9d804873aec78636697b
AlSz <53378802+AlSz@users.noreply.github.com>
2019-08-12 21:32:15 +0200

    Dataset prédiction

---
+++ Alain/Dataset_prediction/GenerationDatasetPrediction.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# Nombre de valeur à générer
+SampleSet=10
+
+# orientation de la ligne avec l'axe
+# en degrés
+AngleMin=-20.
+AngleMax=+20.
+AngleStep=1.
+
+# Décalage de la ligne au démarrage
+# en % de la demi-largeur d'image
+PosMin=-100.
+PosMax=+100.
+PosStep=10.
+
+# Vitesse de la camera
+# equivalent au pas en y
+SpeedMin=1.
+SpeedMax=10.
+SpeedStep=1
+
+# variance des perturbations aléatoires
+Variance=0
+
+DataFileName="predictiondata.txt"
+
+f=open(DataFileName, "w")
+for angle in np.arange(AngleMin, AngleMax+AngleStep, AngleStep) :
+  for pos in np.arange(PosMin, PosMax+PosStep, PosStep) :
+    values=np.zeros(SampleSet+1, dtype=float)
+    speed=1.
+    sortie=""
+    for i in range(0, SampleSet+1):
+      val=pos+i*speed*np.tan(np.deg2rad(angle))
+      sortie=sortie+"{:.2f};".format(val)
+    sortie=sortie+"{:03d};{:03d};".format(int(angle), int(pos))
+    print(sortie, file=f)
+    
+f.close()
