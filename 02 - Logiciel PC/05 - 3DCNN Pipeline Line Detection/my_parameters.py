89dab8769bded754d08f51a0a0e5e034874064ed
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-09-13 20:34:04 +0200

    2D CNN Paul OKKKK

--- 02 - Logiciel PC/05 - 3DCNN Pipeline Line Detection/my_parameters.py
+++ 02 - Logiciel PC/05 - 3DCNN Pipeline Line Detection/my_parameters.py
@@ -1,8 +1,8 @@
 ## PARAMETERS ##################################################################
 
 # inputs/outputs "dataset_002", "dataset_003", "dataset_004",
-dataset_dir = [  "dataset_003", "dataset_004", "dataset_007"  ]     ## add training dataset here
-testset_dir = [  "dataset_008" ]     ## add test dataset here (en attendant, on remet les training sets)
+dataset_dir = [  "dataset_003", "dataset_004", "dataset_007", "dataset_008" ]     ## add training dataset here
+testset_dir = [  "dataset_007" ]     ## add test dataset here (en attendant, on remet les training sets)
 dataset_filename = "dataset.txt"    ## do not change
 label_filename = "label.txt"     ## do not change
 video_dir = "video"    ## do not change
@@ -35,12 +35,15 @@ conv_layers = [
     ('norm', 0),                                  ## do not change
     
     ('conv2D', 8, (5,5), (2,2)),           ## filters, kernel_size, stride
+    ('conv2D', 8, (5,5), (2,2)),           ## filters, kernel_size, stride
+#    ('conv2D', 16, (3,3), (1,1)),           ## filters, kernel_size, stride
     ('maxpooling2D', (2,2), (2,2)),     ## size, stride
     ('dropout',  0.2),
 
 ]
 full_connected_hidden_layers= [
     (8, 'relu',  0.2),         ## default 256 units in 1srt hidden layer, 10% dropout
+#    (8, 'tanh',  0.2),         ## default 256 units in 1srt hidden layer, 10% dropout
 #    (16, 'relu',  0.2)       ## default 128 units in 2nd hidden layer, 10% dropout
 ]      
 
@@ -91,3 +94,23 @@ hyp_patience=20
 ##]  
 ##Epoch 60/60
 ## - 8s - val_mean_squared_error: 0.0030
+
+
+##essais gilles
+### CNN parameters
+##### see build_3d_cnn https://github.com/autorope/donkeycar/blob/dev/donkeycar/parts/keras.py
+##### Credit: https://github.com/jessecha/DNRacing/blob/master/3D_CNN_Model/model.py
+##conv_layers = [
+##    ('crop2D', picture_height_crop),         ## do not change
+##    ('norm', 0),                                  ## do not change
+##    
+##    ('conv2D', 8, (5,5), (2,2)),           ## filters, kernel_size, stride
+##    ('conv2D', 8, (3,15), (2,5)),           ## filters, kernel_size, stride
+##    ('maxpooling2D', (2,2), (2,2)),     ## size, stride
+##    ('dropout',  0.2),
+##
+##]
+##full_connected_hidden_layers= [
+##    (8, 'relu',  0.2),         ## default 256 units in 1srt hidden layer, 10% dropout
+###    (16, 'relu',  0.2)       ## default 128 units in 2nd hidden layer, 10% dropout
+##] 
