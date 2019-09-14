c904c4079d4a988e8565590649e43f3beaeaa216
pat92fr <48148915+pat92fr@users.noreply.github.com>
2019-09-14 11:01:43 +0200

    Clean Up & versionning

--- 02 - Logiciel PC/03 - 3DCNN Pipeline/bkup/README.txt
+++
@@ -1,3 +0,0 @@
-import dataset and testset from OneDrive : 
-https://1drv.ms/u/s!AgPL1p8-kSaRg9RDySAEe-KTSa6sng?e=njtSwe
-
--- 02 - Logiciel PC/03 - 3DCNN Pipeline/bkup/my_contants.py
+++
@@ -1,7 +0,0 @@
-## CONSTANTS ###################################################################
-
-# picture
-picture_initial_width = 160 
-picture_initial_height = 90 
-picture_initial_shape = (picture_initial_height, picture_initial_width, 3)
-
